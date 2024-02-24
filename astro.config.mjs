5f56bdaa3d6a6fb3db822f08bb6bf72d5babf3dc
Ian Mathaiya <iannmathaiya@gmail.com>
2024-02-24 13:38:52 +0300

    added shadcn ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from 'astro/config';
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+})
