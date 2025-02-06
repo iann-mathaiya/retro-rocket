ec41e4b02c88dfa8ac810e4f0a47e9be73d7bd34
Ian Mathaiya <iannmathaiya@gmail.com>
2025-02-06 21:25:47 +0300

    feat: get products list in the client

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -1,15 +1,23 @@
-import { defineAction } from "astro:actions";
 import Stripe from "stripe";
+import { defineAction } from "astro:actions";
+import { z } from "astro:schema";
+
+process.removeAllListeners('warning')
 
 export const products = {
     getProducts: defineAction({
         handler: async(context) => {
             const { env } = context.locals.runtime;
 
-            const stripe = new Stripe(env.STRIPE_SECRET_KEY)
+            const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+                apiVersion: '2025-01-27.acacia',
+                httpClient: Stripe.createFetchHttpClient()
+            })
 
             try {
-                const products = await stripe.products.list();
+                const products = await stripe.products.list({active: true});
+
+                console.log('fdgbdxfbxfbh', products)
 
                   return { success: true, products };
             } catch (error) {
