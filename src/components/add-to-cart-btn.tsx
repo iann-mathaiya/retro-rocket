026a3bc2003101a78a19437235f36ad7bb0b8d78
Ian Mathaiya <iannmathaiya@gmail.com>
2025-02-08 17:07:53 +0300

    refactor: replaces nanostores with jotai

--- src/components/add-to-cart-btn.tsx
+++ src/components/add-to-cart-btn.tsx
@@ -1,13 +1,41 @@
-import { $cart, $cartItems } from '../lib/store';
-import { useStore } from '@nanostores/react';
+import { useAtom } from 'jotai';
+import { cartAtom } from '../lib/store';
 import type { StripeProduct } from '../lib/types';
+import { useEffect } from 'react';
 
 
-export default function AddToCartButton({ product, }: { product: StripeProduct }) {
-  
+export default function AddToCartButton({ product, }: { product: StripeProduct; }) {
+  const [cart, setCart] = useAtom(cartAtom);
+
+  useEffect(() => {
+    setCart(JSON.parse(localStorage.getItem("cart") || "[]"));
+  }, [setCart]);
+
+  function addToCart() {
+    const { id, name, images, default_price } = product;
+
+    const updatedCart = [...cart];
+    const existingItem = updatedCart.find((item) => item.id === product.id);
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      updatedCart.push({
+        id, name, imageSrc: images[0], quantity: 1,
+        price: typeof default_price === 'object' ? Number.parseInt(String(default_price?.unit_price ?? '0')) : (default_price ?? 0),
+      });
+    }
+
+    setCart(updatedCart);
+    localStorage.setItem("cart", JSON.stringify(updatedCart));
+  }
+
+  console.log('cart', cart);
+
   return (
-    <button type='button'
-      className="mt-2 w-fit py-2 px-8 min-h-8 flex items-center justify-center gap-2 text-white bg-gray-950 hover:bg-orange-600 hover:cursor-pointer hover:scale-110 rounded-full transition-all duration-500 ease-in-out">
+    <button type='button' onClick={addToCart}
+      className="mt-2 w-fit py-2 px-8 min-h-8 flex items-center justify-center gap-2 text-white bg-gray-950 hover:bg-orange-600 hover:cursor-pointer hover:scale-110 rounded-full transition-all duration-500 ease-in-out"
+    >
       Add to cart
     </button>
   );
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,21 +1,4 @@
-import { atom, map } from "nanostores";
+import { atom } from "jotai";
 import type { CartItem } from "./types";
 
-export const $cart = atom<CartItem[]>([]);
-export const $cartItems = map<Record<string, CartItem>>({});
-
-export function addCartItem({ id, name, price, imageSrc }: CartItem) {
-    const existingEntry = $cartItems.get()[id];
-    
-    if (existingEntry) {
-        $cartItems.setKey(id, {
-            ...existingEntry,
-            quantity: existingEntry.quantity + 1,
-        });
-    } else {
-        $cartItems.setKey(
-            id,
-            { id, name, imageSrc, price, quantity: 1 }
-        );
-    }
-}
+export const cartAtom = atom<CartItem[]>([]);
