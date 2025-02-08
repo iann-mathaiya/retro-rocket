4d28de1ad0ae1ddf4adac2947332d241fd1121df
Ian Mathaiya <iannmathaiya@gmail.com>
2025-02-08 09:16:26 +0300

    feat: adds retrieve stripe product server action

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -22,16 +22,16 @@ export const products = {
             });
 
             try {
-                const response = await stripe.products.list({ 
+                const response = await stripe.products.list({
                     active: true,
                     expand: ['data.default_price'],
                 });
 
-                console.log(response.data)
+                console.log(response.data);
 
                 if (!response?.data || !Array.isArray(response.data)) {
                     throw new Error('Invalid response format from Stripe');
-                  }
+                }
 
                 const transformedProducts: StripeProduct[] = response.data.map((product) => ({
                     id: product.id,
@@ -40,19 +40,59 @@ export const products = {
                     active: product.active,
                     metadata: product.metadata,
                     description: product.description,
-                    default_price: typeof product.default_price === 'object' ? 
-                    {
-                        unit_price: ((product.default_price?.unit_amount ?? 0) / 100).toFixed(2), 
-                        currency: product.default_price?.currency
-                    }  
-                    : 
-                      product.default_price,
-                  }));
+                    default_price: typeof product.default_price === 'object' ?
+                        {
+                            unit_price: ((product.default_price?.unit_amount ?? 0) / 100).toFixed(2),
+                            currency: product.default_price?.currency
+                        }
+                        :
+                        product.default_price,
+                }));
 
                 return { success: true, products: transformedProducts };
             } catch (error) {
                 console.error(error);
                 return { success: false, message: "An unexpected error occurred. Please try again." };
+            }
+        }
+    }),
+    getProduct: defineAction({
+        input: z.object({
+            productId: z.string()
+        }),
+        handler: async ({ productId }) => {
+
+            const stripe = new Stripe(import.meta.env.STRIPE_SECRET_KEY, {
+                apiVersion: '2025-01-27.acacia',
+                httpClient: Stripe.createFetchHttpClient()
+            });
+
+            try {
+                const product = await stripe.products.retrieve(productId, {
+                    expand: ['default_price'],
+                });
+
+                const formattedProduct = {
+                    ...product,
+                    default_price: typeof product.default_price === 'object' ?
+                        {
+                            unit_price: ((product.default_price?.unit_amount ?? 0) / 100).toFixed(2),
+                            currency: product.default_price?.currency
+                        }
+                        :
+                        product.default_price
+                };
+
+                return { success: true, product: { ...formattedProduct } };
+            } catch (error) {
+                if (error instanceof Stripe.errors.StripeError) {
+                    if (error.type === 'StripeInvalidRequestError') {
+                        return { success: false, message: "Product not found" };
+                    }
+                }
+
+                console.error('Error fetching product:', error);
+                return { success: false, message: "Failed to fetch product details" };
             }
         }
     })
