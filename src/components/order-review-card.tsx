bdf5548dc1fb149e571af4188e30ffb75dc76392
Ian Mathaiya <iannmathaiya@gmail.com>
2025-02-23 01:13:02 +0300

    styling: updates order details

--- src/components/order-review-card.tsx
+++ src/components/order-review-card.tsx
@@ -105,70 +105,76 @@ export default function OrderReviewCard() {
                 Your order is on it's way!
             </p>
 
-            <div className='mt-6 sm:mt-8 space-y-2.5'>
-                <h2 className='text-sm text-gray-900 font-semibold'>Customer Info</h2>
+            <div className='mt-6 sm:mt-8  grid grid-cols-1 sm:grid-cols-3 gap-8'>
+                <div className='space-y-6 sm:space-y-8'>
+                    <div className='space-y-2.5'>
+                        <h2 className='text-sm text-gray-900 font-semibold'>Customer Info</h2>
 
-                <div className='flex items-center gap-3'>
-                    <svg viewBox="0 0 36 36" fill="none" role="img" xmlns="http://www.w3.org/2000/svg" className='size-12'>
-                        <title>Customer Profile Placeholder</title>
-                        <mask id=":r79:" maskUnits="userSpaceOnUse" x="0" y="0" width="36" height="36">
-                            <rect width="36" height="36" rx="72" fill="#FFFFFF" />
-                        </mask>
-                        <g mask="url(#:r79:)">
-                            <rect width="36" height="36" fill="#ff005b" />
-                            <rect x="0" y="0" width="36" height="36" transform="translate(0 0) rotate(324 18 18) scale(1)" fill="#ffb238" rx="36" />
-                            <g transform="translate(-4 -4) rotate(-4 18 18)">
-                                <path d="M15 19c2 1 4 1 6 0" stroke="#000000" fill="none" strokeLinecap="round" />
-                                <rect x="10" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000" />
-                                <rect x="24" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000" />
-                            </g>
-                        </g>
-                    </svg>
+                        <div className='flex items-center gap-3'>
+                            <svg viewBox="0 0 36 36" fill="none" role="img" xmlns="http://www.w3.org/2000/svg" className='size-12'>
+                                <title>Customer Profile Placeholder</title>
+                                <mask id=":r79:" maskUnits="userSpaceOnUse" x="0" y="0" width="36" height="36">
+                                    <rect width="36" height="36" rx="72" fill="#FFFFFF" />
+                                </mask>
+                                <g mask="url(#:r79:)">
+                                    <rect width="36" height="36" fill="#ff005b" />
+                                    <rect x="0" y="0" width="36" height="36" transform="translate(0 0) rotate(324 18 18) scale(1)" fill="#ffb238" rx="36" />
+                                    <g transform="translate(-4 -4) rotate(-4 18 18)">
+                                        <path d="M15 19c2 1 4 1 6 0" stroke="#000000" fill="none" strokeLinecap="round" />
+                                        <rect x="10" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000" />
+                                        <rect x="24" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000" />
+                                    </g>
+                                </g>
+                            </svg>
 
-                    <div>
-                        <h3 className='text-sm font-medium text-gray-900'>{stripeGuestCustomer?.name}</h3>
-                        <p className='text-sm lowercase text-gray-600'>{stripeGuestCustomer?.email}</p>
+                            <div>
+                                <h3 className='text-sm font-medium text-gray-900'>{stripeGuestCustomer?.name}</h3>
+                                <p className='text-sm lowercase text-gray-600'>{stripeGuestCustomer?.email}</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className='space-y-2.5'>
+                        <h2 className='text-sm text-gray-900 font-semibold'>Shipping Info</h2>
+
+                        <div>
+                            <p className='text-sm text-gray-600'>{shippingInfo?.phone}</p>
+                            <p className='text-sm capitalize text-gray-600'>{shippingInfo?.address}</p>
+                            <p className='text-sm capitalize text-gray-600'>{shippingInfo?.city}, {shippingInfo?.country}</p>
+                        </div>
+
                     </div>
                 </div>
 
-            </div>
 
-            <div className='mt-6 sm:mt-8 space-y-2.5'>
-                <h2 className='text-sm text-gray-900 font-semibold'>Shipping Info</h2>
+                <div className='space-y-2.5 sm:order-first sm:col-span-2'>
+                    <h2 className='text-sm text-gray-900 font-semibold'>In your bag</h2>
 
-                <div>
-                    <p className='text-sm text-gray-600'>{shippingInfo?.phone}</p>
-                    <p className='text-sm capitalize text-gray-600'>{shippingInfo?.address}</p>
-                    <p className='text-sm capitalize text-gray-600'>{shippingInfo?.city}, {shippingInfo?.country}</p>
+                    {/* <pre>{JSON.stringify(orderDetails, null, 2)}</pre> */}
+
+                    <ul className='space-y-6 sm:space-y-8'>
+                        {orderDetails?.map(order =>
+                            <li key={order.orderId}>
+                                <h3 className='text-xs text-gray-900 font-semibold'>Order #{order.orderId} - ordered at {order.orderedAt}</h3>
+
+                                {order.items.map(item =>
+                                    <ol key={item.id} className='flex items-center'>
+                                        <img src={item.imageSrc} alt={item.name} className='w-20 aspect-square object-center object-cover' />
+
+                                        <div>
+                                            <h4 className='text-sm text-gray-700'>{item.name}</h4>
+                                            <p className='text-xs text-gray-500'>{item.quantity} x ${item.price}</p>
+                                        </div>
+                                    </ol>
+
+                                )}
+                            </li>
+                        )}
+                    </ul>
                 </div>
-
             </div>
 
-            <div className='mt-6 sm:mt-8 space-y-2.5'>
-                <h2 className='text-sm text-gray-900 font-semibold'>In your bag</h2>
-
-                {/* <pre>{JSON.stringify(orderDetails, null, 2)}</pre> */}
-
-                <ul>
-                    {orderDetails?.map(order =>
-                        <li key={order.orderId}>
-                            <h3 className='text-xs text-gray-900 font-semibold'>Order #{order.orderId} - ordered at {order.orderedAt}</h3>
-
-                            {order.items.map(item =>
-                                <ol key={item.id} className='flex items-center'>
-                                    <img src={item.imageSrc} alt={item.name} className='w-20 aspect-square object-center object-cover' />
-
-                                    <div>
-                                        <h4 className='text-sm text-gray-700'>{item.name}</h4>
-                                        <p className='text-xs text-gray-500'>{item.quantity} x ${item.price}</p>
-                                    </div>
-                                </ol>
-
-                            )}
-                        </li>
-                    )}
-                </ul>
-            </div>
 
         </div>
     );
