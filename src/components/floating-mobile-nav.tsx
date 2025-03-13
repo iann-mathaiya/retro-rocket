8c6bb15793a7748db375a092f4048b03a84b23f5
Ian Mathaiya <iannmathaiya@gmail.com>
2025-03-13 17:05:44 +0300

    refactor: update products link in floating nav

--- src/components/floating-mobile-nav.tsx
+++ src/components/floating-mobile-nav.tsx
@@ -31,7 +31,7 @@ export default function FloatingMobileNavigation() {
               <span>Need help shopping?</span>
             </button>
 
-            <a href='/products' className='w-full p-3 flex items-center gap-3 text-lg text-gray-900 bg-gray-100 hover:bg-orange-600 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
+            <a href='/' className='w-full p-3 flex items-center gap-3 text-lg text-gray-900 bg-gray-100 hover:bg-orange-600 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-gray-500">
                 <title>Box Icon</title>
                 <path d="M12 1L21.5 6.5V17.5L12 23L2.5 17.5V6.5L12 1ZM5.49388 7.0777L12.0001 10.8444L18.5062 7.07774L12 3.311L5.49388 7.0777ZM4.5 8.81329V16.3469L11.0001 20.1101V12.5765L4.5 8.81329ZM13.0001 20.11L19.5 16.3469V8.81337L13.0001 12.5765V20.11Z" />
