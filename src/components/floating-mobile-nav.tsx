6d24c7cc67d0e197b659aa07db7bc37a8e5f0337
Ian Mathaiya <iannmathaiya@gmail.com>
2025-03-13 17:24:42 +0300

    styling: update hover styling of the floating nav actions

--- src/components/floating-mobile-nav.tsx
+++ src/components/floating-mobile-nav.tsx
@@ -14,7 +14,7 @@ export default function FloatingMobileNavigation() {
         <Drawer.Content className="w-full sm:max-w-sm h-fit fixed bottom-0 -translate-x-1/2 left-1/2 outline-none">
           <div className='p-4 m-4 bg-white rounded-3xl'>
             <div className="pb-4 flex items-center justify-between border-b border-gray-200">
-              <h2 className='text-xl text-gray-600'>Options</h2>
+              <h2 className='text-xl sm:text-lg text-gray-600'>Options</h2>
               <Drawer.Close className='p-1.5 bg-gray-100 rounded-full'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-gray-600">
                   <title>Close Icon</title>
@@ -24,7 +24,7 @@ export default function FloatingMobileNavigation() {
             </div>
 
             <div className='mt-4 space-y-3'>
-              <button type="button" className='w-full p-3 flex items-center gap-3 text-lg text-orange-600 bg-orange-200/40 hover:bg-orange-600 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
+              <button type="button" className='w-full p-3 flex items-center gap-3 text-lg sm:text-base text-orange-600 bg-orange-200/40 hover:bg-orange-200/65 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-orange-500">
                   <title>Question Icon</title>
                   <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM13 13.3551V14H11V12.5C11 11.9477 11.4477 11.5 12 11.5C12.8284 11.5 13.5 10.8284 13.5 10C13.5 9.17157 12.8284 8.5 12 8.5C11.2723 8.5 10.6656 9.01823 10.5288 9.70577L8.56731 9.31346C8.88637 7.70919 10.302 6.5 12 6.5C13.933 6.5 15.5 8.067 15.5 10C15.5 11.5855 14.4457 12.9248 13 13.3551Z" />
@@ -32,7 +32,7 @@ export default function FloatingMobileNavigation() {
                 <span>Need help shopping?</span>
               </button>
 
-              <a href='/' className='w-full p-3 flex items-center gap-3 text-lg text-gray-900 bg-gray-100 hover:bg-orange-600 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
+              <a href='/' className='w-full p-3 flex items-center gap-3 text-lg sm:text-base text-gray-900 bg-gray-100 hover:bg-gray-200 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-gray-500">
                   <title>Box Icon</title>
                   <path d="M12 1L21.5 6.5V17.5L12 23L2.5 17.5V6.5L12 1ZM5.49388 7.0777L12.0001 10.8444L18.5062 7.07774L12 3.311L5.49388 7.0777ZM4.5 8.81329V16.3469L11.0001 20.1101V12.5765L4.5 8.81329ZM13.0001 20.11L19.5 16.3469V8.81337L13.0001 12.5765V20.11Z" />
@@ -40,7 +40,7 @@ export default function FloatingMobileNavigation() {
                 <span>Products</span>
               </a>
 
-              <a href='/blog' className='w-full p-3 flex items-center gap-3 text-lg text-gray-900 bg-gray-100 hover:bg-orange-600 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
+              <a href='/blog' className='w-full p-3 flex items-center gap-3 text-lg sm:text-base text-gray-900 bg-gray-100 hover:bg-gray-200 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-gray-500">
                   <title>Article Icon</title>
                   <path d="M20 22H4C3.44772 22 3 21.5523 3 21V3C3 2.44772 3.44772 2 4 2H20C20.5523 2 21 2.44772 21 3V21C21 21.5523 20.5523 22 20 22ZM19 20V4H5V20H19ZM7 6H11V10H7V6ZM7 12H17V14H7V12ZM7 16H17V18H7V16ZM13 7H17V9H13V7Z" />
@@ -48,7 +48,7 @@ export default function FloatingMobileNavigation() {
                 <span>Blog</span>
               </a>
 
-              <a href='/about' className='w-full p-3 flex items-center gap-3 text-lg text-gray-900 bg-gray-100 hover:bg-orange-600 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
+              <a href='/about' className='w-full p-3 flex items-center gap-3 text-lg sm:text-base text-gray-900 bg-gray-100 hover:bg-gray-200 hover:cursor-pointer rounded-xl transition-all duration-500 ease-in-out'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-gray-500">
                   <title>Information Icon</title>
                   <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 7H13V9H11V7ZM11 11H13V17H11V11Z" />
